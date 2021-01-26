c58eb9350ab7e796bbfe9aee45a5415e56d2ca22
Chris Polansky <cpolansky@hashicorp.com>
2021-01-26 22:02:33 +0000

    Adding vars and module

--- examples/chris/main.tf
+++ examples/chris/main.tf
@@ -1,3 +1,17 @@
 provider "tfe" {
   token = var.tfe_token
 }
+
+module "example" {
+  source = "../../"
+
+  config_file_path = "examples/chris"
+  organization     = var.organization
+
+  vcs_repo = {
+    branch             = var.branch
+    identifier         = var.identifier
+    ingress_submodules = var.ingress_submodules
+    oauth_token_id     = var.oauth_token_id
+  }
+}
